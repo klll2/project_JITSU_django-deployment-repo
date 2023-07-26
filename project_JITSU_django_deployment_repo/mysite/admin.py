4d5207b759b7476c8181f1ec799df6f450d634c5
Klll2 <60285296+klll2@users.noreply.github.com>
2023-07-26 21:01:06 +0900

    Update admin.py

--- project_JITSU_django_deployment_repo/mysite/admin.py
+++ project_JITSU_django_deployment_repo/mysite/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Userword, Word
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Userword)
+admin.site.register(Word)
