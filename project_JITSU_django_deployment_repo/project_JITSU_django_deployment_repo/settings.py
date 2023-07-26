396b36aa2fc42ea9ebf8245781208c711f6fdf41
Klll2 <60285296+klll2@users.noreply.github.com>
2023-07-26 23:55:31 +0900

    Update settings.py
    append app's config

--- project_JITSU_django_deployment_repo/project_JITSU_django_deployment_repo/settings.py
+++ project_JITSU_django_deployment_repo/project_JITSU_django_deployment_repo/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'mysite.apps.MysiteConfig',
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
